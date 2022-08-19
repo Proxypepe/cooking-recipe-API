6e5d7e9ab38ab0f11a55d43dda066984ac164aca
Proxypepe <sasha_golovin_95@bk.ru>
2022-08-19 13:54:18 +0300

    added parameter endpoint

---
+++ data.py
@@ -0,0 +1,20 @@
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "",
+    },
+]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from data import RECIPES
 
 app = FastAPI(
     title="Recipe API"
@@ -11,6 +12,13 @@ router = APIRouter()
 @router.get('/', status_code=200)
 def root() -> dict:
     return {"msg": "root endpoint"}
+
+
+@router.get('/recipe/{recipe_id}', status_code=200)
+def get_recipe_by_id(*, recipe_id: int) -> dict:
+    result = [recipe for recipe in RECIPES if recipe['id'] == recipe_id]
+    if result:
+        return result[0]
 
 
 app.include_router(router)
