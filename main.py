cad2007ecfa9df69b0f21c4ae9c967cf78a0e941
Proxypepe <sasha_golovin_95@bk.ru>
2022-08-19 14:03:13 +0300

    added query endpoint

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from fastapi import FastAPI, APIRouter
+
 from data import RECIPES
 
 app = FastAPI(
     title="Recipe API"
 )
-
 
 router = APIRouter()
 
@@ -21,8 +23,20 @@ def get_recipe_by_id(*, recipe_id: int) -> dict:
         return result[0]
 
 
+@router.get('/search/', status_code=200)
+def search_recipe(
+    keyword: Optional[str] = None, max_results: Optional[int] = 10
+) -> dict:
+    if keyword is None:
+        return {'result': RECIPES[:max_results]}
+
+    results = filter(lambda recipe: keyword.lower() in recipe['label'].lower(), RECIPES)
+    return {'result': list(results)[:max_results]}
+
+
 app.include_router(router)
 
 if __name__ == '__main__':
     import uvicorn
+
     uvicorn.run(app, host='localhost', port=8001, log_level="debug")
